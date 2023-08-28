af6dc34d18f8be6578d9a6334305fe50b633d30d
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-28 09:40:53 +0500

    test

--- web/index.php
+++ web/index.php
@@ -12,9 +12,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Origin: https://tranquil-island-01847-9479261fef91.herokuapp.com");
     header("Access-Control-Allow-Headers: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    // Вывод заголовков в консоль
-    var_dump(getallheaders());
-
     exit;
 }
 (new yii\web\Application($config))->run();
