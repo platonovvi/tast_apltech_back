a098ed44471de8123eeb421b95ce7d34a01e5bce
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-28 06:20:23 +0500

    111

--- web/index.php
+++ web/index.php
@@ -8,10 +8,11 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
-
-header("Access-Control-Allow-Origin: https://tranquil-island-01847-9479261fef91.herokuapp.com");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: https://tranquil-island-01847-9479261fef91.herokuapp.com");
+    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    exit;
+}
 (new yii\web\Application($config))->run();
 
