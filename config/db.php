5aef6b8070bd0bd5cdb48b05bbea709b69a822c9
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-28 17:09:13 +0500

    add db config

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => getenv('DATABASE_URL'),
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=ec2-107-21-67-46.compute-1.amazonaws.com;port=5432;dbname=db0asres84leif',
+    'username' => 'aoverokslpmuih',
+    'password' => '82998c11d8c760242578d4ded65fc5c6e46fc9111ade524a47b9e60b4357fa2f',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
