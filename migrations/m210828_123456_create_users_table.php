83610e20dbf615e6aa072c7765193308446b99ac
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-28 17:50:22 +0500

    add_migration_users

---
+++ migrations/m210828_123456_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%users}}`.
+ */
+class m210828_123456_create_users_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $this->createTable('cat_users', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'password' => $this->string()->notNull(),
+            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        $this->dropTable('cat_users');
+    }
+}
