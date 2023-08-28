d00c7005ca82f656164d4a661fa85fbe3fadc0b0
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-28 17:18:57 +0500

    change dsn config

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=ec2-107-21-67-46.compute-1.amazonaws.com;port=5432;dbname=db0asres84leif',
+    'dsn' => getenv('DATABASE_URL'),
     'username' => 'aoverokslpmuih',
     'password' => '82998c11d8c760242578d4ded65fc5c6e46fc9111ade524a47b9e60b4357fa2f',
     'charset' => 'utf8',
