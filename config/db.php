76888e865661b7f68e68ed96aac03e86770831d1
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-30 18:13:41 +0500

    change db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     //'dsn' => getenv('DATABASE_URL'),
-    'dsn' => 'pgsql:host=ec2-107-21-67-46.compute-1.amazonaws.com;port=5432;dbname=db0asre',
+    'dsn' => 'pgsql:host=ec2-107-21-67-46.compute-1.amazonaws.com;port=5432;dbname=db0asres84leif',
     'username' => 'aoverokslpmuih',
     'password' => '82998c11d8c760242578d4ded65fc5c6e46fc9111ade524a47b9e60b4357fa2f',
     'charset' => 'utf8',
