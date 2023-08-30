5c7a1545fae8f617df5a1c32a797f65615afa815
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-31 04:50:53 +0500

    asdasd

--- config/web.php
+++ config/web.php
@@ -7,73 +7,10 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'components' => [
-        'request' => [
-            'cookieValidationKey' => 'vvPB--SLcGS3ZUHxk18vSY_6hNbg75J8',
-        ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'useFileTransport' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => $db,
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-                '' => 'site/hello',
-            ],
-        ],
-    ],
-    'params' => $params,
-];
-
-if (YII_ENV_DEV) {
-    // Конфигурация для разработки
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
-
-return $config;
-/*use yii\filters\Cors;
-
-$params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/db.php';
-
-$config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm' => '@vendor/npm-asset',
     ],
-    'defaultRoute' => 'site/hello',
     'components' => [
         'request' => [
             'cookieValidationKey' => 'vvPB--SLcGS3ZUHxk18vSY_6hNbg75J8',
@@ -84,7 +21,7 @@ $config = [
             ],
         ],
         'response' => [
-            'format' => yii\web\Response::FORMAT_JSON,
+            'format' => 'json',
             'charset' => 'UTF-8',
         ],
         'cache' => [
@@ -124,6 +61,7 @@ $config = [
         ],
 
         'urlManager' => [
+            'class' => 'yii\web\UrlManager',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
@@ -147,4 +85,4 @@ if (YII_ENV_DEV) {
     ];
 }
 
-return $config;*/
+return $config;
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,13 +3,8 @@
 namespace app\controllers;
 
 use Yii;
-use yii\web\Controller;
 class SiteController extends Controller
 {
-    public function actionHello()
-    {
-        return $this->render('hello');
-    }
     public function actionIndex()
     {
         return $this->render('index'); // Возвращаем представление для действия index
