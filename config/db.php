a3647bf33625eda76b2f638d32c79ca2d50842aa
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-30 15:06:17 +0500

    change config db.php

--- config/db.php
+++ config/db.php
@@ -4,15 +4,13 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => getenv('DATABASE_URL'),
     'charset' => 'utf8',
-    // Дополнительные настройки, если необходимо
     'on afterOpen' => function ($event) {
-        // Вы можете добавить дополнительные действия после установки соединения
         Yii::info('Database connection successful.', 'application');
     },
     'schemaMap' => [
         'pgsql' => [
             'class' => 'yii\db\pgsql\Schema',
-            'defaultSchema' => 'public', // Если ваша схема отличается от "public"
+            'defaultSchema' => 'public',
         ],
     ],
 ];
