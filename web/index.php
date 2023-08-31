d7c9b5f92b5c5d81e583ff92bf10755f188be3d2
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-31 11:51:37 +0500

    logs

--- web/index.php
+++ web/index.php
@@ -6,6 +6,18 @@ defined('YII_ENV') or define('YII_ENV', 'prod');
 ini_set('display_errors', 'On');
 ini_set('display_startup_errors', 'On');
 error_reporting(E_ALL);
+
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+$domain = $_SERVER['HTTP_HOST'];
+$port = $_SERVER['SERVER_PORT'];
+
+$baseUrl = $protocol . '://' . $domain;
+if (($protocol === 'http' && $port != 80) || ($protocol === 'https' && $port != 443)) {
+    $baseUrl .= ':' . $port;
+}
+
+error_log('Root URL: ' . $baseUrl); // Запись в лог
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
