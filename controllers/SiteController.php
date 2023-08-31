597c9a7bc8adedea7d01c480fadcd8a2bf2de255
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-31 13:51:55 +0500

    asd

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -12,7 +12,8 @@ class SiteController extends BaseController
 {
     public function actionIndex()
     {
-        Yii::info('Метод actionIndex() был вызван.');
-        return $this->render('index'); // Возвращаем представление для действия index
+        $content = 'Content';
+        return $this->renderContent($content);
+        //return $this->render('index'); // Возвращаем представление для действия index
     }
 }
