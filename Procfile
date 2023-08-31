d8496fa90f6b26d07818bc0954503edebd23310b
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-01 01:40:37 +0500

    transfer URL for query

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-nginx web/index.php
+web: vendor/bin/heroku-php-nginx web/
--- web/index.php
+++ web/index.php
@@ -8,7 +8,6 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
