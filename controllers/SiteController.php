182b617bf02ad9a650601638b47727f8cc98c7ef
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-31 08:54:50 +0500

    error

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,11 +2,101 @@
 
 namespace app\controllers;
 
+use app\models\ContactForm;
+use app\models\LoginForm;
+use yii\web\Controller as BaseController;
 use Yii;
-class SiteController extends Controller
+use yii\web\Response;
+
+class SiteController extends BaseController
 {
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
     public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return Response|string
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return string
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+}
+/*    public function actionIndex()
     {
         return $this->render('index'); // Возвращаем представление для действия index
     }
-}
+}*/
--- views/site/error.php
+++ views/site/error.php
@@ -3,25 +3,34 @@
 /** @var yii\web\View $this */
 /** @var string $name */
 /** @var string $message */
-/** @var Exception$exception */
+
+/** @var Exception $exception */
 
 use yii\helpers\Html;
 
+Yii::error('Error occurred: ' . $exception->getMessage());
 $this->title = $name;
-?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+try {
+    ?>
+    <div class="site-error">
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <h1><?= Html::encode($this->title) ?></h1>
+
+        <div class="alert alert-danger">
+            <?= nl2br(Html::encode($message)) ?>
+        </div>
+
+        <p>
+            The above error occurred while the Web server was processing your request.
+        </p>
+        <p>
+            Please contact us if you think this is a server error. Thank you.
+        </p>
+
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
+    <?php
+} catch (Exception $e) {
+    echo 'An error occurred while rendering the error page: ' . $e->getMessage();
+}
+?>
