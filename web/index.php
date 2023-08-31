9899b4ca4b7470b55e48faa067ff44974e7d73c3
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-31 12:13:25 +0500

    logs

--- web/index.php
+++ web/index.php
@@ -1,25 +1,14 @@
 <?php
-ini_set('log_errors', 1);
-ini_set('error_log', '/tests/php-error.log');
-// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'prod');
+
 ini_set('display_errors', 'On');
 ini_set('display_startup_errors', 'On');
 error_reporting(E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
-$domain = $_SERVER['HTTP_HOST'];
-$port = $_SERVER['SERVER_PORT'];
 
-$baseUrl = $protocol . '://' . $domain;
-if (($protocol === 'http' && $port != 80) || ($protocol === 'https' && $port != 443)) {
-    $baseUrl .= ':' . $port;
-}
-
-error_log('Root URL: ' . $baseUrl);
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
