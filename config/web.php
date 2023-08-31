7c208169fa8f92c361c1ecb2f97899fc56784788
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-31 10:25:52 +0500

    asdad

--- config/web.php
+++ config/web.php
@@ -36,7 +36,16 @@ $config = [
                 Yii::info('Connected to the database!');
             },
         ],*/
-        // ... Другие компоненты ...
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                // ...
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
     ],
     'params' => $params,
 ];
