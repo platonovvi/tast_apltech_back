4600e9a9ccc5560247f3678ddbe6bd6e23f57649
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-04 03:35:08 +0500

    asdew

--- web/index.php
+++ web/index.php
@@ -20,18 +20,3 @@ if (PHP_SAPI === 'cli-server') {
 } else {
     (new yii\web\Application($config))->run();
 }
-
-
-/*defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'prod');
-
-ini_set('display_errors', 'On');
-ini_set('display_startup_errors', 'On');
-error_reporting(E_ALL);
-
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-$config = require __DIR__ . '/../config/web.php';
-
-(new yii\web\Application($config))->run();*/
-
