6746d6872915800af0d01f386d2918cf891d8a77
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-04 03:09:53 +0500

    asdew

--- config/web.php
+++ config/web.php
@@ -1,6 +1,6 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ?: 'prod');
+defined('YII_ENV') or define('YII_ENV', 'prod');
 $params = require __DIR__ . '/params.php';
 
 $config = [
@@ -71,85 +71,3 @@ if (YII_ENV) {
 }
 
 return $config;
-/*$params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/db.php';
-
-$config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'components' => [
-        'request' => [
-            'cookieValidationKey' => 'vvPB--SLcGS3ZUHxk18vSY_6hNbg75J8',
-            'class' => 'yii\web\Request',
-            'enableCsrfValidation' => false,
-            'parsers' => [
-                'application/json' => 'yii\web\JsonParser',
-            ],
-        ],
-        'response' => [
-            'format' => 'json',
-            'charset' => 'UTF-8',
-        ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mailer' => [
-            'class' => \yii\symfonymailer\Mailer::class,
-            'viewPath' => '@app/mail',
-            'useFileTransport' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => getenv('DATABASE_URL'),
-            'username' => getenv('DB_USERNAME'),
-            'password' => getenv('DB_PASSWORD'),
-            'charset' => 'utf8',
-            'enableSchemaCache' => true,
-            'on afterOpen' => function ($event) {
-                Yii::info('Connected to the database!');
-            },
-        ],
-
-        'urlManager' => [
-            'class' => 'yii\web\UrlManager',
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-                'users' => 'user/get-users',
-                '/' => 'site/index',
-            ],
-        ],
-    ],
-    'params' => $params,
-];
-
-if (YII_ENV_DEV) {
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
-
-return $config;*/
