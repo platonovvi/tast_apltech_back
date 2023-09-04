f75878e0fb8edfe75664105a79e6b6f45bb66035
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-04 20:18:14 +0500

    asdas

--- web/index.php
+++ web/index.php
@@ -1,12 +1,7 @@
 <?php
-// Определение корневой директории
 defined('YII_ROOT') or define('YII_ROOT', dirname(__DIR__));
-// Подключаем автозагрузчик Composer
 require(__DIR__ . '/../vendor/autoload.php');
-// Подключаем Yii2
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-
-// Подключение файла конфигурации
 $config = require __DIR__ . '/../config/web.php';
 
 // Определение порта, на котором будет работать встроенный сервер PHP
