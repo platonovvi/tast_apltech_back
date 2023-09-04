4763af639f5c2412a058ccaf863c404c5dd13a12
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-04 20:56:13 +0500

    asd

--- web/index.php
+++ web/index.php
@@ -1,13 +1,20 @@
 <?php
-defined('YII_ROOT') or define('YII_ROOT', dirname(__DIR__));
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-$config = require __DIR__ . '/../config/web.php';
 
-// Определение порта, на котором будет работать встроенный сервер PHP
-$port = getenv('PORT') ?: 8080;
+// Определяем, используется ли встроенный PHP-сервер
+if (PHP_SAPI === 'cli-server') {
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    // Если запрошенный файл существует, возвращаем его
+    if (is_file($file)) {
+        return false;
+    }
+}
 
-// Запуск встроенного сервера PHP
-$host = '0.0.0.0';
-$server = new yii\web\Server($config);
-$server->serve($host, $port);
+// Подключаем автозагрузчик Composer
+require __DIR__ . '/vendor/autoload.php';
+
+// Подключаем файл с настройками Yii2 приложения
+$config = require __DIR__ . '/config/web.php';
+
+// Создаем и запускаем экземпляр Yii2 приложения
+(new yii\web\Application($config))->run();
