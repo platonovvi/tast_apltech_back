dbfca1f1263b371d39a0f9d34d4f2582d33ed978
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-04 21:24:27 +0500

    asd

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
-// index.php в корне
 
-// Определяем базовую директорию вашего приложения
-define('BASE_DIR', __DIR__);
+// Определите путь к файлу autoload.php
+$autoloadPath = __DIR__ . '/app/vendor/autoload.php';
 
-// Перенаправляем запрос к web/index.php
-require(BASE_DIR . '/web/index.php');
+// Проверьте, существует ли autoload.php
+if (!file_exists($autoloadPath)) {
+    die("Ошибка: Файл 'autoload.php' не найден. Убедитесь, что зависимости Composer установлены и верно настроены.");
+}
+
+// Подключите автозагрузчик
+require_once $autoloadPath;
+
+// Определите путь к файлу web/index.php
+$yiiPath = __DIR__ . '/app/web/index.php';
+
+// Проверьте, существует ли index.php
+if (!file_exists($yiiPath)) {
+    die("Ошибка: Файл 'index.php' не найден в директории 'web'. Убедитесь, что структура проекта настроена правильно.");
+}
+
+// Подключите файл index.php
+require_once $yiiPath;
