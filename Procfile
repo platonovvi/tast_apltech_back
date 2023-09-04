2fb6d0a96d49d985ba2c9c559749591e55b448fa
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-04 16:30:39 +0500

    asdas

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: php yii serve --port=$PORT
--- web/index.php
+++ web/index.php
@@ -6,18 +6,13 @@ require(__DIR__ . '/../vendor/autoload.php');
 // Подключаем Yii2
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-// Определение окружения (Development, Production и т.д.)
-/*defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ?: 'prod');*/
 // Подключение файла конфигурации
 $config = require __DIR__ . '/../config/web.php';
-// Запуск приложения Yii2
-if (PHP_SAPI === 'cli-server') {
-    $url = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-} else {
-    (new yii\web\Application($config))->run();
-}
+
+// Определение порта, на котором будет работать встроенный сервер PHP
+$port = getenv('PORT') ?: 8080;
+
+// Запуск встроенного сервера PHP
+$host = '0.0.0.0';
+$server = new yii\web\Server($config);
+$server->serve($host, $port);
