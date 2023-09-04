d1576bdfaa60aa83f5a9f959a7715312c62e405e
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-04 08:19:04 +0500

    asd

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -6,6 +6,7 @@ class SiteController extends BaseController
 {
     public function actionIndex()
     {
+        echo "Маршрут до контроллера SiteController::actionIndex достигнут!";
         return $this->render('index'); // Возвращаем представление для действия index
     }
 }
