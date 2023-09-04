1f97c09856d03aa650db3a6a004584c9811698a0
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-04 08:13:23 +0500

    asd

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,18 +1,11 @@
 <?php
 
 namespace app\controllers;
-
-use app\models\ContactForm;
-use app\models\LoginForm;
-use Yii;
-use yii\web\Response;
-
-class SiteController extends Controller
+use yii\web\Controller as BaseController;
+class SiteController extends BaseController
 {
     public function actionIndex()
     {
-        $content = 'Content';
-        //return $this->renderContent($content);
         return $this->render('index'); // Возвращаем представление для действия index
     }
 }
