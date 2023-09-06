1766ebf55660b857f851bf6f3a75149e6108ec8c
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 18:28:08 +0500

    asdasd

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace app\controllers;
+use app\models\User;
 use yii\web\Controller as BaseController;
 class SiteController extends BaseController
 {
     public function actionIndex()
     {
         echo "Маршрут до контроллера SiteController::actionIndex достигнут!";
-        return $this->render('index'); // Возвращаем представление для действия index
+        $users = User::find()->asArray()->all();
+        return $this->asJson($users);
+        //return $this->render('index'); // Возвращаем представление для действия index
     }
 }
