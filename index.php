dac9e004749c61253ca72e9a411e8db59e1af917
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 15:22:34 +0500

    asdasd

--- index.php
+++ index.php
@@ -7,14 +7,6 @@ require(__DIR__ . '/vendor/autoload.php');
 // Подключаем файл с настройками Yii2 приложения
 $config = require __DIR__ . '/config/web.php';
 //$config = require __DIR__ . '\config\web.php';
-// Определяем, используется ли встроенный PHP-сервер
-if (PHP_SAPI === 'cli-server') {
-    $url = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    // Если запрошенный файл существует, возвращаем его
-    if (is_file($file)) {
-        return false;
-    }
-}
+
 // Создаем и запускаем экземпляр Yii2 приложения
 (new yii\web\Application($config))->run();
