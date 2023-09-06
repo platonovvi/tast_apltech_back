c0da06ab1920736a8bbabee244417cbe91497441
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 22:39:11 +0500

    db

--- web/index.php
+++ web/index.php
@@ -3,18 +3,8 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 use yii\web\Application;
 // Подключаем автозагрузчик Composer
 require(__DIR__ . '/../vendor/autoload.php');
-if (require(__DIR__ . '/../vendor/autoload.php')) {
-    echo "Класс auto определен.";
-} else {
-    echo "Класс auto не определен.";
-}
 // Подключаем файл с настройками Yii2 приложения
 $config = require(__DIR__ . '/../config/web.php');
-if (require(__DIR__ . '/../config/web.php')) {
-    echo "Класс conf определен.";
-} else {
-    echo "Класс conf не определен.";
-}
 // Создаем и запускаем экземпляр Yii2 приложения
 try {
     (new Application($config))->run();
