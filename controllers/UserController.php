1786f292ba04d9a8797bc9a5fed295ee8aba410e
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 21:55:31 +0500

    nginx

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -32,7 +32,7 @@ class UserController extends BaseController
             Yii::$app->db->open();
             echo "Подключение к базе данных успешно установлено.";
         } catch (\yii\db\Exception $e) {
-            echo getenv('DATABASE_URL') . $e->getMessage();
+            echo Yii::$app->db . $e->getMessage();
         }
         //Yii::debug("Маршрут до контроллера UserController::actionGetUsers достигнут!");
         //echo "Действие actionGetUsers вызвано!";
