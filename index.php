bc8d5300eca43f3b59f8563834ab5ae619e499c3
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 15:43:13 +0500

    asdasd

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-if (require(__DIR__ . '/vendor/autoload.php')) {
+// Подключаем автозагрузчик Composer
+require(__DIR__ . '/vendor/autoload.php');
+//require(__DIR__ . '\vendor\autoload.php');
+if (require (__DIR__ . '/config/web.php')) {
     echo 'autoload.php успешно загружен.';
 } else {
     die('Ошибка: autoload.php не загружен.');
 }
-// Подключаем автозагрузчик Composer
-require(__DIR__ . '/vendor/autoload.php');
-//require(__DIR__ . '\vendor\autoload.php');
-
 // Подключаем файл с настройками Yii2 приложения
-$config = require __DIR__ . '/config/web.php';
+$config = require (__DIR__ . '/config/web.php');
 //$config = require __DIR__ . '\config\web.php';
 
 // Создаем и запускаем экземпляр Yii2 приложения
