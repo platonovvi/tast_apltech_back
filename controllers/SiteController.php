c28e9ee1caa1359d9abe70a18ec36575c15e2269
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 18:53:06 +0500

    nginx

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace app\controllers;
 use app\models\User;
 use yii\web\Controller as BaseController;
@@ -7,7 +6,13 @@ class SiteController extends BaseController
 {
     public function actionIndex()
     {
-        Yii::debug("Маршрут до контроллера SiteController::actionIndex достигнут!");
+        //Yii::debug("Маршрут до контроллера SiteController::actionIndex достигнут!");
+        try {
+            Yii::$app->db->open();
+            echo 'Соединение с базой данных установлено успешно!';
+        } catch (\yii\db\Exception $e) {
+            echo 'Ошибка подключения к базе данных: ' . $e->getMessage();
+        }
         $users = User::find()->asArray()->all();
         return $this->asJson($users);
 
