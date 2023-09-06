4b8e9648e72bb0da2fa316b2f4ea21540966f178
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 15:15:41 +0500

    asdasd

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+// Подключаем автозагрузчик Composer
+require(__DIR__ . '/vendor/autoload.php');
+//require(__DIR__ . '\vendor\autoload.php');
+
+// Подключаем файл с настройками Yii2 приложения
+$config = require __DIR__ . '/config/web.php';
+//$config = require __DIR__ . '\config\web.php';
 // Определяем, используется ли встроенный PHP-сервер
 if (PHP_SAPI === 'cli-server') {
     $url = parse_url($_SERVER['REQUEST_URI']);
@@ -9,14 +16,5 @@ if (PHP_SAPI === 'cli-server') {
         return false;
     }
 }
-
-// Подключаем автозагрузчик Composer
-require(__DIR__ . '/vendor/autoload.php');
-//require(__DIR__ . '\vendor\autoload.php');
-
-// Подключаем файл с настройками Yii2 приложения
-$config = require __DIR__ . '/config/web.php';
-//$config = require __DIR__ . '\config\web.php';
-
 // Создаем и запускаем экземпляр Yii2 приложения
 (new yii\web\Application($config))->run();
