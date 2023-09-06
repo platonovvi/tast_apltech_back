9a64ddea3c1c559a44894d90c8c089941d21dfbc
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 19:02:42 +0500

    nginx

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,12 +1,11 @@
 <?php
 namespace app\controllers;
-use app\models\User;
 use yii\web\Controller as BaseController;
 class SiteController extends BaseController
 {
     public function actionIndex()
     {
-        //Yii::debug("Маршрут до контроллера SiteController::actionIndex достигнут!");
+        Yii::debug("Маршрут до контроллера SiteController::actionIndex достигнут!");
         return $this->render('index'); // Возвращаем представление для действия index
     }
 }
